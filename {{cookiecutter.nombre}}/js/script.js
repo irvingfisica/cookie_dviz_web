f91d4105bddb0d92d788ea3cd8b680773b562746
Irving Morales <irvingfisica@gmail.com>
2024-07-17 19:10:00 -0600

    mapa
    mapa agregado

--- {{cookiecutter.nombre}}/js/script.js
+++ {{cookiecutter.nombre}}/js/script.js
@@ -1,12 +1,19 @@
 import * as douze from "../libs/douze/douze.js";
 
 const div = document.querySelector("#graf");
+let mapa = new douze.Mapa("mapa")
+    .set_aspect(0.8);
 
-const datap = [d3.json({{cookiecutter.data}})];
+const datap = [d3.json({{cookiecutter.data}}),
+    d3.buffer("./resources/datos/ent_18.json.gz"),];
 
 Promise.all(datap).then(function(datap) {
 
     let data = datap[0];
+    let ents = douze.geofflprocess(datap[1])[0];
+
+    let ently = new douze.Layer(mapa, ents, "estados");
+    ently.geopaths.style("stroke-width",0.3);
 
     const plot = Plot.plot({
         color: {legend:true},

afaf25107661c755dfe753ca27b32d11a65196fc
Irving Morales <irvingfisica@gmail.com>
2024-12-03 09:23:54 -0600

    mapa fuera
    Quité el mapa porque se generaba con leaflet+d3js

--- {{cookiecutter.nombre}}/js/script.js
+++ {{cookiecutter.nombre}}/js/script.js
@@ -1,19 +1,24 @@
-import * as douze from "../libs/douze/douze.js";
+let width = parseInt(d3.select("#graf").style("width"));
+let height = window.innerWidth > 576 ? width*0.56 : height*1.5;
+
+let margin = {top: 0, bottom: 0, right: 0, left: 0};
+let effective_width = width - margin.left - margin.right;
+let effective_height = height - margin.top - margin.bottom;
+
+let svg = d3.select("#grafsvg").append("svg")
+            .attr("width", effective_width + margin.left + margin.right)
+            .attr("height", effective_height + margin.top + margin.bottom)
+            .append("g")
+            .attr("class","gb_svg")
+            .attr("transform","translate(" + margin.left + "," + margin.top + ")");
 
 const div = document.querySelector("#graf");
-let mapa = new douze.Mapa("mapa")
-    .set_aspect(0.8);
 
-const datap = [d3.json({{cookiecutter.data}}),
-    d3.buffer("./resources/datos/ent_18.json.gz"),];
+const datap = [d3.json({{cookiecutter.data}})];
 
 Promise.all(datap).then(function(datap) {
 
     let data = datap[0];
-    let ents = douze.geofflprocess(datap[1])[0];
-
-    let ently = new douze.Layer(mapa, ents, "estados");
-    ently.geopaths.style("stroke-width",0.3);
 
     const plot = Plot.plot({
         color: {legend:true},

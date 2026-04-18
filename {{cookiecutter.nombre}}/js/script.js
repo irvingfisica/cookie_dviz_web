f72b2dca5572116d21ef72e91688af7661addc98
Irving Morales <irvingfisica@gmail.com>
2026-04-18 10:19:25 -0600

    fuera bootstrap, hola grid

--- {{cookiecutter.nombre}}/js/script.js
+++ {{cookiecutter.nombre}}/js/script.js
@@ -1,16 +1,19 @@
-let width = parseInt(d3.select("#grafsvg").style("width"));
-let height = window.innerWidth > 576 ? width*0.56 : height*1.5;
+const container = d3.select("#grafica-sector");
+const { width, height } = container.node().getBoundingClientRect();
 
-let margin = {top: 0, bottom: 0, right: 0, left: 0};
-let effective_width = width - margin.left - margin.right;
-let effective_height = height - margin.top - margin.bottom;
+// Importante: El margen de D3 debe "restar" espacio, no sumarlo
+const margin = { top: 20, right: 20, bottom: 40, left: 50 };
+const innerWidth = width - margin.left - margin.right;
+const innerHeight = height - margin.top - margin.bottom;
 
-let svg = d3.select("#grafsvg").append("svg")
-            .attr("width", effective_width + margin.left + margin.right)
-            .attr("height", effective_height + margin.top + margin.bottom)
-            .append("g")
-            .attr("class","gb_svg")
-            .attr("transform","translate(" + margin.left + "," + margin.top + ")");
+const svg = container.append("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", `0 0 ${width} ${height}`)
+    .style("display", "block"); // Evita el espacio extra de los inline-elements
+
+const g = svg.append("g")
+    .attr("transform", `translate(${margin.left},${margin.top})`);
 
 const datap = [d3.json({{cookiecutter.data}})];
 
@@ -18,5 +21,13 @@ Promise.all(datap).then(function(datap) {
 
   let data = datap[0];
   console.log(data);
+
+  const xScale = d3.scaleLinear()
+      .domain([0, d3.max(data, d => d.valor)])
+      .range([0, innerWidth]);
+
+  const yScale = d3.scaleBand()
+      .domain(data.map(d => d.label))
+      .range([0, innerHeight]);
 
 })
